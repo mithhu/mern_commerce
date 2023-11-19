25af723f39d89f6de19b29a723a370f917ff2e8c
mithhu <mithhu666@gmail.com>
2023-11-20 02:32:46 +0800

    added frontend product page and cart

---
+++ frontend/src/constants.js
@@ -0,0 +1,3 @@
+export const BASE_URL =
+  process.env.NODE_ENV === 'develeopment' ? 'http://localhost:5001' : '';
+export const PRODUCTS_URL = '/api/products';
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,33 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 import {
   createBrowserRouter,
   createRoutesFromElements,
   Route,
   RouterProvider,
-} from 'react-router-dom';
-import HomeScreen from './screens/HomeScreen';
+} from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store";
+import HomeScreen from "./screens/HomeScreen";
 // import 'bootstrap/dist/css/bootstrap.min.css';
 // import `index.css`
-import './assets/styles/bootstrap.custom.css';
-import './assets/styles/index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import ProductScreen from './screens/ProductScreen';
+import "./assets/styles/bootstrap.custom.css";
+import "./assets/styles/index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import ProductScreen from "./screens/ProductScreen";
+import CartScreen from "./screens/CartScreen";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path='/' element={<App />}>
-      <Route index={true} path='/' element={<HomeScreen />} />
-      <Route path='/product/:id' element={<ProductScreen />} />
+    <Route path="/" element={<App />}>
+      <Route index={true} path="/" element={<HomeScreen />} />
+      <Route path="/product/:id" element={<ProductScreen />} />
+      <Route path="/cart" element={<CartScreen />} />
     </Route>
   )
 );
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} /> // Add this line
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>
 );
 reportWebVitals();
