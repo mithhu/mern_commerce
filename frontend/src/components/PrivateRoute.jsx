9fdef77a1315443077f7241cacc33da940a6d384
mithhu <mithhu666@gmail.com>
2023-11-24 02:41:25 +0800

    payment

---
+++ frontend/src/components/PrivateRoute.jsx
@@ -0,0 +1,9 @@
+import { Navigate, Outlet } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+
+const PrivateRoute = () => {
+  const { userInfo } = useSelector((state) => state.auth);
+  console.log(userInfo);
+  return userInfo ? <Outlet /> : <Navigate to='/login' replace />;
+};
+export default PrivateRoute;
